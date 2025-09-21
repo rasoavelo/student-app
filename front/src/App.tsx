f4d69acd014e87c9332f9130622d3378568e43e8
Helene <helenerasoavelo@gmail.com>
2025-09-22 00:14:50 +0300

    Feat: initial project front

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import Logo from "./components/logo";
+function App() {
+  return (
+    <>
+      <div>
+        <Logo />
+        <h1 className="text-gray-900 text-4xl text-center pt-5">
+          Liste des etudiants
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
